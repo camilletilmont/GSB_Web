dddffaf2dc0ee9280fb5e9b1b17f38b7cf6236a0
Clayton Georges <camille.tilmont@hotmail.fr>
2019-05-28 09:50:47 +0200

    V1.1 avec coms

--- logout.php
+++ logout.php
@@ -1,10 +1,20 @@
 <?php
+
+//page realisant la destruction des informations de Sessions et de cookies lors de la déconnexion
+
+
+//lance un test d'existence d'une session'
 include('test_connecter.php');
-echo '<meta http-equiv="refresh" content="0.1;url=./connexion.php"/>';
+
+
+
+//si la session actuelle a décroché on la réveille
 if(!isset($_SESSION))
 {
 	session_start();
 }
+
+//mis à zéro et destruction des valeurs de sessions
 $_SESSION = array();
 unset($_SESSION["nom"]);
 unset($_SESSION["matricule"]);
@@ -12,5 +22,8 @@ setcookie("nom", "", 1);
 setcookie("matricule", "", 1);
 session_destroy();
 
+
+//redirection vers la page connexion
+echo '<meta http-equiv="refresh" content="0.1;url=./connexion.php"/>';
 
  ?>
--- test_connecter.php
+++ test_connecter.php
@@ -1,10 +1,12 @@
 <?php
 	//Permet de vérifier si l'utilisateur est connecté, s'il ne l'est pas alors il est redirigé vers la page de connexion.
-	
+
+	//vérifie si une session existe, au cas ou l'actuel se serait endormie elle la réveille
 	if(!isset($_SESSION))
 	{
 		session_start();
 	}
+	//Si il n'y a pas de session active (et donc pas de valeur associé à la clé 'nom') alors redirection evrs la page de co
 	if (!(isset($_SESSION["nom"])))
 	{
 		echo '<script>window.location.replace("./connexion.php");</script>';
